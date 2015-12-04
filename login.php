893cfdcc5ab5d7d2c1d6ae866f21559d0cbe5ce9
liteng <liteng@monstar-lab.cn>
2015-12-04 12:39:42 +0800

    add byliteng 12.30

--- login.php
+++ login.php
@@ -1,8 +1,52 @@
-<?php
-echo "Hello,world!";
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<html>
+    <body>
+        <?php
+$name=$_POST["name"];
+$password=$_POST["password"];
+if ( !(isset($name)||isset($password))  ){  
+?>
+ <h1>Please Log IN</h1>
+<form method='post' action='login.php'>
+<p>name:<input type="text" name="name"></p> 
+<p>password:<input type="text" name="password"></p>
+        <p><input type="submit" name="submit" value="登录"></p>
+</form>
+
+
+        <?php
+}else {
+    $mysql=mysqli_connect("localhost","root","root");
+    if(!mysql){
+        echo 'wrong 1';
+        exit;
+    }
+    $selected=mysqli_select_db($mysql,"cloud_picture");
+    if(!$selected){
+        echo "wrong 2";
+        exit;    
+    }
+    $query="select count(*)from t_user where
+        name='".$name."'and
+            password='".$password."'";           
+     $result=mysqli_query($mysql,$query);
+         if(!$result){
+             echo 'wrong 3';
+             exit;
+             }
+     $row=mysqli_fetch_row($result);
+         $count=$row[0];
+             if($count>0){
+                  echo 'Welcome!'."<br>".$name."!";
+                 
+                     }else {
+                     echo 'Go away!';
+                     }                   
+}
+?>
+</body>
+</html>
+
+
+
+
 
